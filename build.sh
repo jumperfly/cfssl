869a138a610af2a05364a2252f2cb518b2bcecfc
jumperfly <git@jumperfly.co.uk>
2018-11-17 16:12:00 +0000

    Set rpm release number as build number

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RPM_RELEASE=$1
+echo "########## Starting build for release $RPM_RELEASE ##########"
+
 echo "########## Clean and prepare build directory ##########"
 if [ -e cfssl ]; then
   rm -rf cfssl
@@ -18,9 +21,16 @@ cd ..
 echo "########## Build cfssl ##########"
 for cmd in cfssl cfssljson; do
   echo -n "Building $cmd... "
-  docker run --rm -v $(pwd)/cfssl:/go/src/github.com/cloudflare/cfssl -v $(pwd)/bin/go/usr/bin:/go/bin golang:1.11.2 go install github.com/cloudflare/cfssl/cmd/$cmd
+  docker run --rm -e RPM_RELEASE=$RPM_RELEASE \
+    -v $(pwd)/cfssl:/go/src/github.com/cloudflare/cfssl \
+    -v $(pwd)/bin/go/usr/bin:/go/bin golang:1.11.2 \
+    go install github.com/cloudflare/cfssl/cmd/$cmd
   echo done.
 done
 
 echo "########## Build rpm ##########"
-docker run --rm -v $(pwd):/build jumperfly/rpmbuild:v4.11.3_1 rpmbuild -bb --buildroot /build/bin/go --define "_topdir /build/bin/rpmbuild" /build/cfssl.spec
+docker run --rm -v $(pwd):/build jumperfly/rpmbuild:v4.11.3_1 \
+  rpmbuild -bb --buildroot /build/bin/go \
+  --define "_topdir /build/bin/rpmbuild" \
+  --define "_release $RPM_RELEASE" \
+  /build/cfssl.spec
--- cfssl.spec
+++ cfssl.spec
@@ -1,10 +1,6 @@
-%define        __spec_install_post %{nil}
-%define          debug_package %{nil}
-%define        __os_install_post %{_dbpath}/brp-compress
-
 Name:           cfssl
 Version:        1.3.2
-Release:        1%{?dist}
+Release:        %{_release}%{?dist}
 Summary:        cfssl
 
 License:	BSD 2-Clause "Simplified" License
